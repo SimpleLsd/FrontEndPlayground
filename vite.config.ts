6063f444d0b501c3e2b1630e6b6533ccf01f3982
SimpleLsd <2497606230@qq.com>
2025-09-04 09:35:12 +0800

    取消lock文件的gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,6 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `@use "~/styles/element/index.scss" as *;`,
-        api: 'modern-compiler',
       },
     },
   },
